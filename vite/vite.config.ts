3795028b6bfa86e80a6ba5d98fefe3bc54fec7ae
Hikaru Moriyama <moriyama.hikaru@intecs.ne.jp>
2025-05-29 14:35:27 +0900

    chore: setup vite environments

---
+++ vite/vite.config.ts
@@ -0,0 +1,25 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    root: resolve(__dirname, 'src'),
+    build: {
+        outDir: resolve(__dirname, '../app/flaskr/static'),
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.ts'),
+            },
+            output: {
+                entryFileNames: 'index.js',
+                assetFileNames: 'style.css',
+            },
+        },
+        minify: true,
+        cssMinify: true,
+    },
+    plugins: [
+        tailwindcss(),
+    ],
+});
